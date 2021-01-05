f64ea4e008a205e6fb4326e21a6e5d5aafcb31f2
Jorge Cabral <jorgect99@gmail.com>
2021-01-05 12:37:12 -0700

    ModelSerializer

--- newsapi/news/api/serializers.py
+++ newsapi/news/api/serializers.py
@@ -1,43 +1,73 @@
+from datetime import datetime
+from django.utils.timesince import timesince
 from rest_framework import serializers
 from news.models import Article
 
-class ArticleSerializers(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
-    title = serializers.CharField()
-    description = serializers.CharField()
-    body = serializers.CharField()
-    location = serializers.CharField()
-    publication_date = serializers.DateField()
-    active = serializers.BooleanField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
+class ArticleSerializer(serializers.ModelSerializer):
     
+    time_since_publication = serializers.SerializerMethodField()
 
-    def create(self, validated_data):
-        print(validated_data)
-        return Article.objects.create(**validated_data)
+    class Meta:
+        model = Article
+        exclude = ("id",)
+        #fields = ("title","description","body") #we want to choose a couple of fields!
+        # fields = "__all__" #all the fields of our model
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author', instance.author)
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.body = validated_data.get('body', instance.body)
-        instance.location = validated_data.get('location', instance.location)
-        instance.publication_date = validated_data.get('publication_date', instance.publication_date)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-
-        return instance
+    def get_time_since_publication(self, object):
+        publication_date = object.publication_date
+        now = datetime.now()
+        time_delta = timesince(publication_date, now)
+        return time_delta
 
     def validate(self, data):
-        
         #check that description and title are different
         if data["title"] == data["description"]:
             raise serializers.ValidationError("Title and description must be different from one another!")
         return data
     
     def validate_title(self, value):
-        if len(value) < 60:
-            raise serializers.ValidationError("The title has to be at least 60 characters long!")
-        return value
+        if len(value) < 30:
+            raise serializers.ValidationError("The title has to be at least 30 characters long!")
+        return value    
+
+
+# class ArticleSerializers(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     body = serializers.CharField()
+#     location = serializers.CharField()
+#     publication_date = serializers.DateField()
+#     active = serializers.BooleanField()
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
+    
+
+#     def create(self, validated_data):
+#         print(validated_data)
+#         return Article.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.location = validated_data.get('location', instance.location)
+#         instance.publication_date = validated_data.get('publication_date', instance.publication_date)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+
+#         return instance
+
+#     def validate(self, data):
+        
+#         #check that description and title are different
+#         if data["title"] == data["description"]:
+#             raise serializers.ValidationError("Title and description must be different from one another!")
+#         return data
+    
+#     def validate_title(self, value):
+#         if len(value) < 30:
+#             raise serializers.ValidationError("The title has to be at least 30 characters long!")
+#         return value
--- newsapi/news/api/views.py
+++ newsapi/news/api/views.py
@@ -5,58 +5,17 @@ from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 
 from news.models import Article
-from news.api.serializers import ArticleSerializers
-
-
-@api_view(["GET", "POST"])
-def article_list_create_api_view(request):
-
-    if request.method == "GET":
-        articles = Article.objects.filter(active=True)
-        serializer = ArticleSerializers(articles, many=True)
-        return Response(serializer.data)
-
-    elif request.method == "POST":
-        serializer = ArticleSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(["GET", "PUT", "DELETE"])
-def article_detail_api_view(request, pk):
-    try:
-        article = Article.objects.get(pk=pk)
-    except Article.DoesNotExist:
-        return Response({"error":{
-                            "code": 404,
-                            "message":"Article not found!"
-                        }}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = ArticleSerializers(article)
-        return Response(serializer.data)
-    
-    elif request.method == "PUT":
-        serializer = ArticleSerializers(article, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data) 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == "DELETE":
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+from news.api.serializers import ArticleSerializer
 
 class ArticleListCreateAPIView(APIView):
 
     def get(self, request):
         articles = Article.objects.filter(active=True)
-        serializer = ArticleSerializers(articles, many=True)
+        serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = ArticleSerializers(data=request.data)
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -70,12 +29,12 @@ class ArticleDetailAPIView(APIView):
     
     def get(self, request, pk):
         article = self.get_object(pk)
-        serializer = ArticleSerializers(article)
+        serializer = ArticleSerializer(article)
         return Response(serializer.data)
     
     def put(self, request, pk):
         article = self.get_object(pk)
-        serializer = ArticleSerializers(article, data = request.data)
+        serializer = ArticleSerializer(article, data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data) 
@@ -85,3 +44,44 @@ class ArticleDetailAPIView(APIView):
         article = self.get_object(pk)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(["GET", "POST"])
+# def article_list_create_api_view(request):
+
+#     if request.method == "GET":
+#         articles = Article.objects.filter(active=True)
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == "POST":
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(["GET", "PUT", "DELETE"])
+# def article_detail_api_view(request, pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
+#     except Article.DoesNotExist:
+#         return Response({"error":{
+#                             "code": 404,
+#                             "message":"Article not found!"
+#                         }}, status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == "GET":
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+    
+#     elif request.method == "PUT":
+#         serializer = ArticleSerializer(article, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data) 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     elif request.method == "DELETE":
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
